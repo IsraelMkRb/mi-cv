1b7e4fdcde82245e93b5000304d181e4b355ad25
IsraelMkRb <imorquecho10@gmail.com>
2025-02-09 00:10:20 -0600

    Avances en diseño

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Description from "./components/description/description";
 import Header from "./components/header/header";
+import Projects from "./components/projects/projects";
 
 function App() {
   return (
     <main className="bg-gradient-to-t from-slate-200 to-slate-50 h-screen overflow-auto">
       <Header />
       <Description />
+      <Projects />
     </main>
   );
 }
---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,30 @@
+const Projects = () => {
+  return (
+    <div className="w-10/12 mx-auto mt-6 fade-up-delay-700 mb-12 grid grid-cols-6 lg:gap-1 rounded-md">
+      <p className="p-4 roboto-flex-400 col-span-6 lg:col-span-3 border border-neutral-400 lg:rounded-lg bg-white shadow-md shadow-zinc-500">
+        <span className="markazi-text-700 text-zinc-700 text-2xl">
+          <strong>Chat y chatbot Whatsapp</strong>
+        </span>
+        <br />
+        <br />
+        Desarrollé un chatbot de WhatsApp integrado con un CRM utilizando React,
+        Next.js y ASP.NET Framework, diseñado para optimizar la atención al
+        cliente y la gestión de leads. Implementé la API de Vonage para
+        gestionar la comunicación en tiempo real y utilicé Botpress como motor
+        de inteligencia artificial para crear flujos de conversación
+        inteligentes y personalizados. El chatbot permitió automatizar
+        respuestas a consultas frecuentes, programar citas y sincronizar datos
+        con el CRM, mejorando la eficiencia operativa y la experiencia del
+        usuario. Gracias a esta solución,
+      </p>
+
+      <img
+        alt="crm-wa"
+        src="/crm-wa.png"
+        className="w-full h-full col-span-6 lg:col-span-3 lg:rounded-md shadow-md shadow-zinc-500"
+      />
+    </div>
+  );
+};
+
+export default Projects;
