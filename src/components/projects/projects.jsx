90a70431c0943f5845dd925692c77e37e9436330
IsraelMkRb <imorquecho10@gmail.com>
2025-02-09 01:06:32 -0600

    Ajustes en responsive

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,7 +1,7 @@
 const Projects = () => {
   return (
     <div className="w-10/12 mx-auto mt-6 fade-up-delay-700 mb-12 grid grid-cols-6 lg:gap-1 rounded-md">
-      <p className="p-4 roboto-flex-400 col-span-6 lg:col-span-3 border border-neutral-400 lg:rounded-lg bg-white shadow-md shadow-zinc-500">
+      <p className="p-4 roboto-flex-400 col-span-6 lg:col-span-3 border border-neutral-400 rounded-t-lg lg:rounded-lg bg-white shadow-md shadow-zinc-500">
         <span className="markazi-text-700 text-zinc-700 text-2xl">
           <strong>Chat y chatbot Whatsapp</strong>
         </span>
@@ -13,15 +13,15 @@ const Projects = () => {
         gestionar la comunicación en tiempo real y utilicé Botpress como motor
         de inteligencia artificial para crear flujos de conversación
         inteligentes y personalizados. El chatbot permitió automatizar
-        respuestas a consultas frecuentes, programar citas y sincronizar datos
+        respuestas a consultas frecuentes, programar entregas y cotizar producto
         con el CRM, mejorando la eficiencia operativa y la experiencia del
-        usuario. Gracias a esta solución,
+        usuario.
       </p>
 
       <img
         alt="crm-wa"
         src="/crm-wa.png"
-        className="w-full h-full col-span-6 lg:col-span-3 lg:rounded-md shadow-md shadow-zinc-500"
+        className="w-full h-full col-span-6 lg:col-span-3 rounded-b-lg lg:rounded-md shadow-md shadow-zinc-500"
       />
     </div>
   );
