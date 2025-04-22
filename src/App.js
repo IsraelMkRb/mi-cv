e38fc3a1df7676e9fea13c162b76062f1a65afa0
IsraelMkRb <imorquecho10@gmail.com>
2025-04-21 22:24:34 -0600

    Se ajusta comportamiento en responsive

--- src/App.js
+++ src/App.js
@@ -43,7 +43,53 @@ function App() {
 
             <p className="text-2xl text-white inter-400 p-4">Acerca de mi...</p>
             <br />
-            <p className="text-white inter-200 p-4">{currentPage}</p>
+            <p className="text-white font-mono p-4">{currentPage}</p>
+            <button
+                onClick={handleNextClick}
+                className="rounded-full border border/white float-right animation-next mb-2 mr-3"
+              >
+                <svg
+                  className="w-6 h-6 text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 12H5m14 0-4 4m4-4-4-4"
+                  />
+                </svg>
+              </button>
+              {currentNumPage > 0 && (
+                <button
+                  className="rounded-full border border/white float-right animation-next mb-2 mr-2"
+                  onClick={handleBeforeClick}
+                >
+                  <svg
+                    class="w-6 h-6 text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 12h14M5 12l4-4m-4 4 4 4"
+                    />
+                  </svg>
+                </button>
+              )}
           </div>
         </div>
 
