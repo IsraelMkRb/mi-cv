e84074b2a8e8eb89ce159b714255b1d7f48d24c9
IsraelMkRb <imorquecho10@gmail.com>
2025-05-03 16:09:39 -0600

    Se agrega boton proyectos en vista movil

--- src/App.js
+++ src/App.js
@@ -49,30 +49,50 @@ function App() {
             <p className="text-white texto-descripcion p-4">
               {<currentPage.elementoTexto />}
             </p>
-            {currentNumPage + 1 < arrayPages.length && (
-              <button
-                onClick={handleNextClick}
-                className="rounded-full border border/white float-right animation-next mb-2 mr-3"
-              >
-                <svg
-                  className="w-6 h-6 text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
+            {currentNumPage + 1 < arrayPages.length ? (
+                <button
+                  onClick={handleNextClick}
+                  className="rounded-full border border/white float-right animation-next mb-2 mr-3"
                 >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M19 12H5m14 0-4 4m4-4-4-4"
-                  />
-                </svg>
-              </button>
-            )}
+                  <svg
+                    className="w-6 h-6 text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 12H5m14 0-4 4m4-4-4-4"
+                    />
+                  </svg>
+                </button>
+              ) : (
+                <button className="rounded-full border border/white float-right animation-next mb-2 mr-3">
+                  <svg
+                    class="w-6 h-6 text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m8 8-4 4 4 4m8 0 4-4-4-4m-2-3-4 14"
+                    />
+                  </svg>
+                </button>
+              )}
             {currentNumPage > 0 && (
               <button
                 className="rounded-full border border/white float-right animation-next mb-2 mr-2"
